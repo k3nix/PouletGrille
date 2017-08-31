11babc67a093335025850035bf6863d52db9fe32
Simon Cappelaere <simonman@hotmail.fr>
2017-08-31 02:39:59 +0200

    test branche simon

--- developpeur.cpp
+++ developpeur.cpp
@@ -11,7 +11,8 @@ void clignioteG()
 		digitalWrite(44, HIGH);
 		delay(1000);
 		digitalWrite(44, LOW);
-		delay(500); //modif 1
+		delay(500); //modif 12 pout le compare
+		//branche simon 
 	} while (true);
 	
 }
