5174d8cad3dd4ac41939b1e262693c85fd4fba03
Simon Cappelaere <simonman@hotmail.fr>
2017-09-01 03:15:14 +0200

    adri
    adri

--- PouletGrille.ino
+++ PouletGrille.ino
@@ -28,7 +28,7 @@ byte rowPins[4] = { 7, 6, 5, 4 }; //connect to the row pinouts of the keypad
 byte colPins[3] = { 3 , 2 , 1 }; //connect to the column pinouts of the keypad
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, 4, 3);
 
-char tempsDePartie[6] = { 0,0,1,5,0,0 }; //Par defaut 15 minutes
+
 
 
 
@@ -42,6 +42,9 @@ void setup() //fonction lancée une seule fois au demmarage de l'arduino
 	LCD.print("Version 0.4");
 	delay(500);
 =======
+	LCD.setCursor(0, 1); // on passe a la deuciemme ligne
+	LCD.print("Version 0.3");
+	delay(2000);
 >>>>>>> 73d04f57fac2b5d91e25feba03da3e69848f7a1a
 
 	LCD.clear();
@@ -65,234 +68,12 @@ void setup() //fonction lancée une seule fois au demmarage de l'arduino
 void loop()
 {
 	
-	//setupTempsDePartie();
-	
-	//clignioteG();
-
-	//modeTest(); //mode de test des boutons
-
-	//choixJeu();
+	setupTempsDePartie();
 	
 	
 
 }
 
-void modeTest()
-{
-	if (digitalRead(pinBT1) > 0)
-	{
-		if (digitalRead(pinBT2) > 0)
-		{
-			LCD.clear();
-			LCD.setCursor(0, 0);
-			LCD.print("Mode TEST");
-			delay(2000);
-			bool finTest = false;
-
-			while (finTest == false)
-			{
-				LCD.clear();
-
-				if (digitalRead(pinBT1) > 0)
-				{
-					if (digitalRead(pinBT2) > 0)
-					{
-						LCD.clear();
-						LCD.setCursor(0, 0);
-						LCD.print("Fin du Test");
-						delay(2000);
-						finTest = true;
-					}
-					LCD.clear();
-					LCD.print("Mode Cles");
-					delay(50);
-
-				}
-
-				if (digitalRead(pinBT2) > 0)
-				{
-					LCD.clear();
-					LCD.print("Mode Code");
-					delay(50);
-				}
-
-				if (digitalRead(pinBT3) > 0)
-				{
-					LCD.clear();
-					LCD.print("Mode Fils");
-					delay(50);
-				}
-
-				if (digitalRead(pinBT4) > 0)
-				{
-					LCD.clear();
-					LCD.print("Equipe Bleue");
-					delay(50);
-				}
-
-				if (digitalRead(pinBT5) > 0)
-				{
-					LCD.clear();
-					LCD.print("Equipe Orange");
-					delay(50);
-				}
-
-				if (digitalRead(pinCLE1) > 0)
-				{
-					LCD.clear();
-					LCD.print("Cle 1");
-					delay(50);
-				}
-
-				if (digitalRead(pinCLE2) > 0)
-				{
-					LCD.clear();
-					LCD.print("Cle 2");
-					delay(50);
-				}
-
-				if (digitalRead(pinCLE3) > 0)
-				{
-					LCD.clear();
-					LCD.print("Cle 3");
-					delay(50);
-				}
-
-				char key = keypad.getKey(); // affiche le keypad
-
-				if (key != NO_KEY)//si une touche est préssée
-				{
-					LCD.print(key);
-					delay(500);
-				}
-
-			}
-		}
-	}
-}
-
-bool choixJeu()
-{
-	int fin = 0;
-	int T_boutons[3];
-	T_boutons[0] = pinBT1;
-	T_boutons[1] = pinBT2;
-	T_boutons[2] = pinBT3;
-
-	bool modeCle = false;
-	bool modeCode = false;
-	bool modeFils = false;
-
-	LCD.clear();
-	LCD.setCursor(0, 0);
-	LCD.print("Mode de jeu ?");
-	delay(200);
-
-	while (fin == 0)
-	{
-		char key = keypad.getKey();
-
-		if (key != NO_KEY)//si une touche est préssée
-		{
-			switch (key)
-			{
-			case '#':
-				{
-					LCD.clear();
-					LCD.setCursor(0, 0);
-					LCD.print("Choix Valides");
-					delay(1000);
-					LCD.clear();
-					exit;
-					break;
-				}
-			default:
-				break;
-			}
-			
-		}
-
-		if (digitalRead(pinBT1) > 0)
-		{
-			if (modeCle == true)
-			{
-				LCD.setCursor(0, 2);
-				LCD.print("Cles Desactive  ");
-				modeCle = false;
-				delay(2000);
-				LCD.setCursor(0, 2);
-				LCD.print("                ");
-			}
-			else
-			{
-				LCD.setCursor(0, 2);
-				LCD.print("Cles Active     ");
-				modeCle = true;
-				delay(2000);
-				LCD.setCursor(0, 2);
-				LCD.print("                ");
-			}
-		}
-
-		if (digitalRead(pinBT2) > 0)
-		{
-			if (modeCode == true)
-			{
-				LCD.setCursor(0, 2);
-				LCD.print("Code Desactive  ");
-				modeCle = false;
-				delay(2000);
-				LCD.setCursor(0, 2);
-				LCD.print("                ");
-			}
-			else
-			{
-				LCD.setCursor(0, 2);
-				LCD.print("Code Active     ");
-				modeCode = true;
-				delay(2000);
-				LCD.setCursor(0, 2);
-				LCD.print("                ");
-			}
-		}
-
-		if (digitalRead(pinBT3) > 0)
-		{
-			if (modeFils == true)
-			{
-				LCD.setCursor(0, 2);
-				LCD.print("Fils Desactive  ");
-				modeFils = false;
-				delay(2000);
-				LCD.setCursor(0, 2);
-				LCD.print("                ");
-			}
-			else
-			{
-				LCD.setCursor(0, 2);
-				LCD.print("Fils Active     ");
-				modeFils = true;
-				delay(2000);
-				LCD.setCursor(0, 2);
-				LCD.print("                ");
-			}
-		}
-
-		if (digitalRead(pinBT5) > 0) //valider
-				{
-					
-					fin = 1;
-					delay(2000);
-				} 
-
-		if (digitalRead(pinBT1) && digitalRead(pinBT2)) // mode test
-		{
-			modeTest();
-		}
-	}
 
 
-
-	return modeCle, modeCode, modeFils;
-}
 
